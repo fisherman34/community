428e06b4ea8e1c1a6046096f8180702ea9f47b33
Sam <fisherman34@qq.com>
2024-05-03 12:27:17 +0800

    extract github config from code to properties

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 ## 码匠社区
 
 ## 资料
-https://spring.io/guides
-https://spring.io/guides/gs/serving-web-content
-https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account
-https://elasticsearch.cn/
+[Sprint 文档](https://spring.io/guides)
+[Sprint Web](https://spring.io/guides/gs/serving-web-content)
+[Github SSH access](https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account)
+[ES](https://elasticsearch.cn/)
+[BootStrap](https://getbootstrap.com/)
+[Github OAuth](https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/creating-an-oauth-app)
 
 ## 工具
-https://git-scm.com/
+[Git](https://git-scm.com/)
+[Visual Paradigm](https://www.visual-paradigm.com/)
---
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -0,0 +1,56 @@
+package life.majiang.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import okhttp3.*;
+
+import org.springframework.stereotype.Component;
+
+
+import java.io.IOException;
+import java.sql.SQLOutput;
+
+/**
+ * @author Sam
+ * @create 2024-04-29 9:53 PM
+ */
+@Component
+public class GithubProvider {
+  public String getAccessToken(AccessTokenDTO accessTokenDTO){
+    MediaType mediaType = MediaType.get("application/json");
+
+    OkHttpClient client = new OkHttpClient();
+
+    RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+    Request request = new Request.Builder()
+              .url("https://github.com/login/oauth/access_token")
+              .post(body)
+              .build();
+      try (Response response = client.newCall(request).execute()) {
+        String string = response.body().string();
+        String token = string.split("&")[0].split("=")[1];
+        return token;
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+      return null;
+  }
+
+  public GithubUser getUser(String accessToken){
+    String token = "Bearer " + accessToken;
+    OkHttpClient client = new OkHttpClient();
+    Request request = new Request.Builder()
+            .url("https://api.github.com/user")
+            .header("Authorization", token)
+            .build();
+    try {
+      Response response = client.newCall(request).execute();
+      String string = response.body().string();
+      GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+      return githubUser;
+    } catch (IOException e) {
+    }
+    return null;
+  }
+}
