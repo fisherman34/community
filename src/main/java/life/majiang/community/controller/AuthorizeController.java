f8654201bdc4fbb4c4ec6ccafcb5aeed1cc17a00
Sam <fisherman34@qq.com>
2024-05-29 15:43:36 +0800

    add check cookies

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -38,7 +39,7 @@ public class AuthorizeController {
   @GetMapping("/callback")
   public String callback(@RequestParam(name = "code") String code,
                          @RequestParam(name = "state") String state,
-                         HttpServletRequest request) {
+                         HttpServletResponse response) {
     AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
     accessTokenDTO.setClient_id(clientId);
     accessTokenDTO.setClient_secret(clientSecret);
@@ -47,16 +48,18 @@ public class AuthorizeController {
 //    accessTokenDTO.setState(state);
     String accessToken = githubProvider.getAccessToken(accessTokenDTO);
     GithubUser githubUser = githubProvider.getUser(accessToken);
+//    System.out.println(githubUser);
     if (githubUser != null){
       User user = new User();
-      user.setToken(UUID.randomUUID().toString());
+      String token = UUID.randomUUID().toString();
+      user.setToken(token);
       user.setName(githubUser.getName());
       user.setAccountId(String.valueOf(githubUser.getId()));
       user.setGmtCreate(System.currentTimeMillis());
       user.setGmtModified(user.getGmtCreate());
       userMapper.insert(user);
-      //登录成功，写cookie 和 session
-      request.getSession().setAttribute("user", githubUser);
+      /** 返回浏览器的信息，用response设置 */
+      response.addCookie(new Cookie("token", token));
       return "redirect:/";
     } else {
       //登录失败
--- src/main/java/life/majiang/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -3,13 +3,34 @@ package life.majiang.community.mapper;
 import life.majiang.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
+ * mapping between SQL databases and objects in Java
  * @author Sam
  * @create 2024-05-03 9:46 PM
  */
 @Mapper
 public interface UserMapper {
+
+  /** This method is annotated with @Insert, which indicates that it will execute
+   * an SQL INSERT statement. The SQL statement provided as the annotation’s value
+   * will insert a new record into the UserTable with the values provided by the User
+   * object’s properties. The #{} syntax is used for parameter substitution, where
+   * MyBatis will replace these placeholders with the corresponding properties from
+   * the User object passed to the method. MyBatis automatically uses the Java object’s
+   * property names as placeholders within the SQL statement. When you pass a single
+   * object parameter to a MyBatis mapper method, like the User object in this case,
+   * MyBatis uses reflection to match the object’s field names with the named placeholders
+   * in the annotated SQL statement.*/
   @Insert("insert into UserTable (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
   void insert(User user);
+
+  /** This method is annotated with @Select, which indicates that it will execute an
+   * SQL SELECT statement. The SQL statement provided will select a user from the user
+   * table where the token column matches the method’s token parameter. The @Param
+   * annotation is used to bind the method parameter to the SQL statement’s placeholder. */
+  @Select("select * from UserTable where token = #{token}")
+  User findByToken(@Param("token") String token);
 }
